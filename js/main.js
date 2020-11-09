99316e14abfe545c0a9a65c6acf4f5e377ff00f6
sirkapil <kapil.math.hons@gmail.com>
2020-11-09 14:26:26 +0530

    Mr Hyde was here …

--- js/main.js
+++ js/main.js
@@ -137,38 +137,33 @@
 	};
 
 	// Set the date we're counting down to
-	var countDownDate = new Date("Nov 30, 2020 15:37:25").getTime();
-	// Update the count down every 1 second
-	var x = setInterval(function() {
+var countDownDate = new Date("Jan 5, 2021 15:37:25").getTime();
 
-	// Get todays date and time
-	var now = new Date().getTime();
+// Update the count down every 1 second
+var x = setInterval(function() {
 
-	// Find the distance between now an the count down date
-	var distance = countDownDate - now;
+  // Get today's date and time
+  var now = new Date().getTime();
 
-	// Time calculations for days, hours, minutes and seconds
-	var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-	var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-	var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-	var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
 
-	// Display the result in an element with id="demo"
-	// document.getElementById("demo").innerHTML = days + "Days " + hours + "Hours "
-	// + minutes + "Minutes " + seconds + "Seconds ";
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-	// Display the result in an element with id="demo"
-	document.getElementById("days").innerHTML = days +" <small>days</small>";
-	document.getElementById("hours").innerHTML = hours + " <small>hours</small> ";
-	document.getElementById("minutes").innerHTML = minutes + " <small>minutes</small> ";
-	document.getElementById("seconds").innerHTML = seconds + " <small>seconds</small> ";
+  // Display the result in the element with id="demo"
+  document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
 
-	// If the count down is finished, write some text 
-	if (distance < 0) {
-	 clearInterval(x);
-	 document.getElementById("countdown").innerHTML = "The Wedding Ceremony is Over";
-	}
-	}, 1000);
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("fh5co-countdown").innerHTML = "EXPIRED";
+  }
+}, 1000);
 
 	// Document on load.
 
